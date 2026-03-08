74770d823893f3ec33a20015b8b94c9fe341330d
Thilak-shelby <wht532@haw-hamburg.de>
2026-03-08 01:05:49 +0100

    Added additional user and authenticated login system

--- src/main/java/com/example/e_commerce/repository/ProductRepository.java
+++ src/main/java/com/example/e_commerce/repository/ProductRepository.java
@@ -2,9 +2,11 @@ package com.example.e_commerce.repository;
 
 import com.example.e_commerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long>  {
 
 }
